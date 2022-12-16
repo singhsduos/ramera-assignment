34c7239e55b287682ecdd9d409f4228f66062da6
singhsduos <neelesh.singh@jalantechnologies.com>
2022-12-16 14:34:48 +0530

    created - rameraExpansion-section

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -3,12 +3,16 @@ import "./landingPage.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faChevronDown,
-  faMagnifyingGlass
+  faMagnifyingGlass,
+  faCubes
 } from "@fortawesome/free-solid-svg-icons";
-
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const LandingPage = () => {
-
   return (
     <>
       <header className="header">
@@ -35,7 +39,7 @@ const LandingPage = () => {
                     />
                   </a>
                 </li>
-                <li class="dropdown">
+                <li className="dropdown">
                   <a href="#companies">
                     OUR COMPANIES
                     <FontAwesomeIcon
@@ -63,7 +67,45 @@ const LandingPage = () => {
             </ul>
           </nav>
         </div>
+        <div className="intro__container">
+          <div className="social_media">
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon icon={faFacebookF} />
+          </div>
+          <div className="company__intro">
+            <h1>Ramaera</h1>
+            <h1>Industries</h1>
+            <h2>
+              An incredible multi-industrial approach oriented towards financial
+              independence, customer focus and serving the best quality to the
+              people.
+            </h2>
+            <button>Explore More</button>
+          </div>
+        </div>
       </header>
+
+      <main className="main">
+        <section className="ramaera__expansion">
+          <div className="ramaera__div">
+            <img
+              src={require("../../resources/images/big_polygon.png")}
+              alt=""
+              className="polygon__img"
+            />
+            <div className="ramaera__heading__div">
+              <h1>The Ramaera Expansion</h1>
+              <h2>
+                We are aiming to be a part of the major industrialisation drive
+                in India and embarking our presence in every significant
+                industrial domain including IT.
+              </h2>
+            </div>
+          </div>
+        </section>
+
+      </main>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./fonts/zilap_orion_fonts/ZilapOrionPersonalUse.ttf"
 import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
